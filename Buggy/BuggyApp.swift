20ffd9f8feab1450fd09aafab58ea82e204f8189
Mike Wasauskee <m68476521.1@gmail.com>
2020-10-20 17:43:12 -0700

    Initial Commit

---
+++ Buggy/BuggyApp.swift
@@ -0,0 +1,17 @@
+//
+//  BuggyApp.swift
+//  Buggy
+//
+//  Created by Miguel Orozco on 10/20/20.
+//
+
+import SwiftUI
+
+@main
+struct BuggyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
